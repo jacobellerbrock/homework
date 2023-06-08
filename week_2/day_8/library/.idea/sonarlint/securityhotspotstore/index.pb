5842181e9dbc928149764c5dfb39b7579e9e0501
Jacob Ellerbrock <ja.eller14@gmail.com>
2023-06-08 13:57:42 +0100

    upload week_2/day_9 homework

--- week_2/day_8/library/.idea/sonarlint/securityhotspotstore/index.pb
+++ week_2/day_8/library/.idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,12 @@
 package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
 J
 src/components/BookForm.js,0\d\0db213bb59949f8c7464da6d6888df98317fe868
+H
+src/firebase/firebase.js,b\8\b8e7bced474903988789d37bcb88d2106df0073c
+B
+src/models/book.js,7\7\7714ecae47d272bc1a6871418f883790a1ff5ca3
+:
+
+src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
+L
+src/services/book-service.js,0\9\099fc648708934ba47e9f8bd5613acad272314c0
---
+++ week_2/day_9/library/src/components/book/BookPage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {useEffect, useState} from "react";
+import LibraryService from "../../services/book-service";
+import {Book} from "../../models/book";
+import BookForm from "./BookForm";
+import BookTable from "./BookTable";
+
+export default function BookPage() {
+
+const [books, setBooks] = useState([]);
+const [bookToEdit, setBookToEdit] = useState(null);
+
+useEffect(
+    () => {
+        if (!books.length) {
+            onInitialLoad();
+        }
+    },[]);
+
+async function onInitialLoad() {
+    try {
+        const books = await LibraryService.fetchBooks();
+        setBooks(books);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function onBookCreated(book) {
+    // setBookToEdit(null);
+    // setBooks([...books, book]);
+    const newbook = await LibraryService.createBook(new Book(null, book.title, book.author, book.isbn));
+    setBooks([...books, newbook]);
+}
+
+async function onBookDelete(book) {
+    // setBooks(books.filter((x) => x.isbn !== book.isbn));
+    await LibraryService.deleteBook(book.id);
+    setBooks(books.filter((x) => book.id !== x.id));
+}
+
+async function onBookEdit(book) {
+    // setBookToEdit(book);
+    // setBooks(books.filter((x) => x.isbn !== book.isbn));
+    await LibraryService.updateBook(book);
+    setBooks(books.filter((x) => book.id!== x.id));
+}
+
+return (
+    <div className={"text-center"}>
+        <div className={'card p-4'}>
+            <BookForm onBookCreated={onBookCreated} bookToEdit={bookToEdit}></BookForm>
+            <BookTable books={books}
+                       onBookDelete={onBookDelete}
+                       onBookEdit={onBookEdit}></BookTable>
+        </div>
+    </div>
+);
+}
